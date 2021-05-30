c9d0f183ea9f3e8ccd7acf24cc74cd0b2cb17b40
abdullahnegm <abdullahahmed1997@hotmail.com>
2021-05-30 13:15:07 +0200

    User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_many :posts
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :name, presence: true, length: { in: 3..25 }, uniqueness: { case_sensitive: false }
+    validates :age, presence: true , numericality: { only_integer: true, greater_than_or_equal_to: 18 }
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 100 }
 end
